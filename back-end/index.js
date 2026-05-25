a53fab26a21625aa2bd708e522354f34e3af7e9e
Dante Luong <dantel1@uci.edu>
2026-05-24 21:16:39 -0700

    Add connection to MongoDB server

---
+++ back-end/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
