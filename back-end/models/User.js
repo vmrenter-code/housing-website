2fd949fffeeafe54c30206eec01dc9c2f34373a2
Dante Luong <dantel1@uci.edu>
2026-05-31 18:20:33 -0700

    update user APIs.

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -60,9 +60,8 @@ userSchema.pre('save', async function (next) {
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
-        next();
     } catch (err) {
-        next(err);
+        throw (err)
     }
 });
 
